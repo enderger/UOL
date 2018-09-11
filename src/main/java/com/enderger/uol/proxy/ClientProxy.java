acb47288cb7e9e6bd662acadc2dd814cc093e651
Enderger <endergeryt@gmail.com>
2018-09-11 14:36:28 -0500

    Worked on ores

--- src/main/java/com/enderger/uol/proxy/ClientProxy.java
+++ src/main/java/com/enderger/uol/proxy/ClientProxy.java
@@ -13,7 +13,7 @@ public class ClientProxy extends CommonProxy{
 
     @Override
     public void registerItemRenderer(Item item, int meta, String id) {
-        ModelLoader.setCustomModelResourceLocation(item, meta, new ModelResourceLocation(Objects.requireNonNull(item.getRegistryName()), id));
+        ModelLoader.setCustomModelResourceLocation(item, meta, new ModelResourceLocation(item.getRegistryName(), id));
     }
     @Override
     public void registerVariantRenderer(Item item, int meta, String filename, String id) {
