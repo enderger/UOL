60530a6dddd5b14aac43e4fbb331575aff14931b
Enderger <endergeryt@gmail.com>
2018-09-11 14:19:37 -0500

    Worked on ores

--- src/main/java/com/enderger/uol/proxy/ClientProxy.java
+++ src/main/java/com/enderger/uol/proxy/ClientProxy.java
@@ -7,11 +7,13 @@ import net.minecraft.item.Item;
 import net.minecraft.util.ResourceLocation;
 import net.minecraftforge.client.model.ModelLoader;
 
+import java.util.Objects;
+
 public class ClientProxy extends CommonProxy{
 
     @Override
     public void registerItemRenderer(Item item, int meta, String id) {
-        ModelLoader.setCustomModelResourceLocation(item, meta, new ModelResourceLocation(item.getRegistryName(), id));
+        ModelLoader.setCustomModelResourceLocation(item, meta, new ModelResourceLocation(Objects.requireNonNull(item.getRegistryName()), id));
     }
     @Override
     public void registerVariantRenderer(Item item, int meta, String filename, String id) {
